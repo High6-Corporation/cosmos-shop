1acd5d33da1ca9d95579aef4773a78e00f1e7166
SSKiyan25 <20-1-01709@vsu.edu.ph>
2026-07-21 18:21:27 +0800

    fix: remove stale onQuickAdd prop reference

--- apps/storefront/src/modules/cart/components/cart-sheet/index.tsx
+++ apps/storefront/src/modules/cart/components/cart-sheet/index.tsx
@@ -186,7 +186,6 @@ export default function CartSheet() {
                     <CartSheetRecommended
                       cart={cart}
                       countryCode={countryCode}
-                      onQuickAdd={setQuickAddProduct}
                     />
                   </div>
                 </>
